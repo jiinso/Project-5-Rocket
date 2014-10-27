c435c40449e79b53978e9b27c44e84648169d25c
jiinso <Jiin@sojiin-ui-MacBook-Air.local>
2014-10-27 00:31:07 -0400

    Vpython
    Finished Vpython… but unsure if this is exactly how we should do it. Do
    you think we need to somehow incorporate the velocity? Also, I tried
    working on the “being creative” part to slow down the rocket to land at
    the ISS but I kind of failed. First wanted to check this part with you.

--- Main.py
+++ Main.py
@@ -7,6 +7,9 @@
 # import needed modules of python
 import numpy as np
 import pylab as pl
+import visual as vis
+ 
+#sphere, cone, display, color, vector, rate, velocity
 
 # assign constant variables with their respective values
 # units are in S.I. units.
@@ -21,6 +24,8 @@ drag_coefficient = 0.5
 m_0 = mass_empty + mass_maxfuel
 
 
+
+
 #----------------------------- Part 1: Rocket Thrust-----------------------------------------------------------------------------------------------------------------------------------------------------------
 # First define the acceleration function that is dependent on time t.
 # We know this equation because we know force = mass * acceleration = burn_rate * v_exhaust
@@ -31,7 +36,7 @@ def acc(t):
 
 # set initial conditions
 a = 0.0							# starting time point
-b = 140.0							# ending time point
+b = 200							# ending time point
 N = 10000						# number of steps
 h = (b-a)/float(N)					# width of each step interval of time
 t_points= np.arange(a, b, h)			# create array of t_points
@@ -49,17 +54,19 @@ for t in t_points:								# for each time point in t_points
         velocity_points.append(velocity_sync)
         velocity += h*acc(t)
         height += h*velocity
+#         if t > 99.9:
+#             print height
     else:										# if t is greater than 100s, the fuel is used up; velocity does not change.
         height_points.append(height)
         velocity_points.append(velocity)				# append the final velocity at 100s for each of the continuing points
-        height += h*velocity						# use the constant velocity to calculate the new height for 100 more seconds.	
+        height += h*velocity						# use the constant velocity to calculate the new height for 100 more seconds.
+
 
 
 # plot graph of height vs. time
 pl.figure(1)
 pl.plot(t_points, height_points, label = 'height')
 pl.legend(loc=2)
-pl.show()
 
 # plot graph of velocity vs. time
 pl.figure(2)
@@ -69,29 +76,21 @@ pl.show()
 
 
 
-
-
 #----------------------------- Part 2: Gravity -----------------------------------------------------------------------------------------------------------------------------------------------------------
 # The acceleration we acquired above did not take gravity into account.
-# But we know that F_total = F_thrust + F_gravity = mass * acceleration - mass * gravity
-# = burn_rate * v_exhaust - burn_rate * t * g_s / (1 + height/R_earth)^2
+# But we know that F_total = F_thrust + F_gravity = mass * acceleration - mass * gravity = burn_rate * v_exhaust - burn_rate * t * g_s / (1 + height/R_earth)^2
 # Note that the mass is not the initial mass, but it is changing (losing burn_rate * t kg of mass as time t changes)
 
-R_earth = 6.37 * 10 ** 6                                # in meters
-Grav_Const = 6.674 * 10 ** -11                          # in N*m^2/kg^2
-M_earth = 5.9722 * 10 ** 24                             # in kilograms
+R_earth = 6.37 * 10 ** 6                              	  # in meters
+Grav_Const = 6.674 * 10 ** -11                    	  # in N*m^2/kg^2
+M_earth = 5.9722 * 10 ** 24                            # in kilograms
 Grav_earth = 9.81
 
 def acc_g(t):
     return (burn_rate * v_exhaust - burn_rate * t * Grav_earth / (1 + height/R_earth) ** 2) / (m_0 - burn_rate*t)
 
-# set initial conditions again for including Gravity
-a = 0.0							# starting time point
-b = 200.0				                # ending time point
-N = 10000						# number of steps
-h = (b-a)/float(N)					# width of each step interval of time
-t_points= np.arange(a, b, h)			        # create array of t_points
-height = 0.0						# initial height, velocity, and time = 0
+# set initial conditions again (things that need to be set to 0 or empty again)
+height = 0.0							# initial height, velocity, and time = 0
 velocity = 0.0
 t_i= 0.0
 velocity = velocity - 0.5*h*acc(t_i)		        # set the velocity half step back for the leap frog method
@@ -115,13 +114,15 @@ for t in t_points:					# for each time point in t_points
 pl.figure(3)
 pl.plot(t_points, height_points, label = 'height')
 pl.legend(loc=2)
-pl.show()
 
 # plot graph of velocity vs. time
 pl.figure(4)
 pl.plot(t_points, velocity_points, label = 'velocity')
 pl.legend(loc=2)
 pl.show()
+
+
+
 
 #----------------------------- Part 3: Air Resistance-----------------------------------------------------------------------------------------------------------------------------------------------------------
 # The acceleration we acquired above did not take Air Resistance into account.
@@ -135,16 +136,9 @@ Temp_Lapse = 0.0065
 Press_Sea = 1.01 * 10 ** 5
 Molar_mass = 0.02896
 Ideal_Gas = 8.314
-height = 0.0
-velocity = 0.0
 
-# set initial conditions
-a = 0.0							# starting time point
-b = 200.0				                # ending time point
-N = 10000						# number of steps
-h = (b-a)/float(N)					# width of each step interval of time
-t_points= np.arange(a, b, h)			        # create array of t_points
-height = 0.0						# initial height, velocity, and time = 0
+# set initial conditions (things that need to be set to 0 or empty again)
+height = 0.0							# initial height, velocity, and time = 0
 velocity = 0.0
 t_i= 0.0
 velocity = velocity - 0.5*h*acc(t_i)		        # set the velocity half step back for the leap frog method
@@ -190,10 +184,32 @@ for t in t_points:					# for each time point in t_points
 pl.figure(5)
 pl.plot(t_points, height_points, label = 'height')
 pl.legend(loc=2)
-pl.show()
 
 # plot graph of velocity vs. time
 pl.figure(6)
 pl.plot(t_points, velocity_points, label = 'velocity')
 pl.legend(loc=2)
 pl.show()
+
+
+
+
+
+#----------------------------- Part 4: VPython -----------------------------------------------------------------------------------------------------------------------------------------------------------
+# Create an animation for the launched rocket reaching the ISS 
+scene1 = vis.display (title = 'Animation of Launched Rocket to the ISS: MaxFuel')
+
+# create three objects: Earth, the International Space Station, and the rocket.
+Earth = vis.sphere(pos=[0,-250,0], radius = 50, color=vis.color.green)
+ISS = vis.sphere(pos = [0, 135, 0], radius = 5, color=vis.color.magenta)
+rocket = vis.cone(pos=[0, -200, 0], axis = (0, 10, 0), radius = 2, color=vis.color.cyan)
+
+# define the rate of the animation and the height of the rocket using the data from the previous parts (heigh_points)
+for item in height_points:
+    vis.rate (1000)
+    rocket.pos.y = (item/1000) - 200
+
+
+
+
+
